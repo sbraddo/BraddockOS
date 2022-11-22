bfa1fe9c470b34fa2a308cb966a8173bd73bae22
sbraddo <81383505+sbraddo@users.noreply.github.com>
2022-11-22 00:24:34 -0500

    Update timer.c

--- 2Signals/timer.c
+++ 2Signals/timer.c
@@ -19,40 +19,41 @@ UPDATE YOUR MAKEFILE
 #include <signal.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <time.h>
 
-bool sig = true;
+bool sig = false;
 int totalAlarms = 0;
-// double totalTime;
-// clock_t start, end;
+double totalTime;
+clock_t start, end;
 
-void handler(int signum)
+void sigAlarmHandler(int signum)
 { //signal handler
-  if (signum == SIGINT){
-    // end = clock();
-    // totalTime = ((double)end - start)/CLOCKS_PER_SEC;
-    printf("Total Alarms: %d \n", totalAlarms);
-    // printf("Total Time in Seconds: %f \n", totalTime);
-    exit(0);
-  }
-  else
+  sig = true;
     printf("Hello World!\n");
     alarm(1);
     totalAlarms++;
-  //exit(1); //exit after printing
-  //wait(0);
+  
+}
+
+void sigIntHandler (int signum){
+  end = clock();
+  totalTime = ((double)end - start)/CLOCKS_PER_SEC;
+  // printf("\n Total Alarms: %d \n", totalAlarms);
+  printf("Total Time in Seconds: %f \n", totalTime);
+  exit(0);
 }
 
 int main(int argc, char * argv[])
 {
-  // start = clock();
-  signal(SIGINT,handler);
-  signal(SIGALRM,handler); //register handler to handle SIGALRM
+  start = clock();
+  signal(SIGALRM,sigAlarmHandler);
+  signal(SIGINT,sigIntHandler); //register handler to handle SIGALRM
   alarm(1); //Schedule a SIGALRM for 1 second
-  totalAlarms++;
-  while(sig){
-    sleep(1);
-    //totalAlarms++;
-    printf("Turning was right!\n");
+  while(1) {
+      sig = false;
+      while(sig == false);
+      //sig = false;
+      printf("Turning was right!\n");
   } //busy wait for signal to be delivered
   return 0; //never reached
 }
